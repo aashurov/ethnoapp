6615dac24d1f088cd5d2736d4f94804e0c5aa096
Abdugani Temirboev <abdugani.temirboev@kdb.uz>
2023-02-17 14:41:11 +0500

    Asalo

---
+++ wwwroot/js/listitem.js
@@ -0,0 +1,12 @@
+function listOfItems() {
+    $(function () {
+        //Initialize Select2 Elements
+        $('.select2').select2()
+
+        //Initialize Select2 Elements
+        $('.select2bs4').select2({
+            theme: 'bootstrap4'
+        })
+
+    })
+}
